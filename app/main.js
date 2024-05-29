14ec44922978171abefd0f41a363f6e04f48b2a6
Tharun Raj <tharun.2.linux@gmail.com>
2024-05-29 10:22:45 +0530

    add "flowing conversations" section

--- app/main.js
+++ app/main.js
@@ -1,2 +1,7 @@
 import "./scss/style.scss";
 import "./style.css";
+
+import validator from "validator";
+import isEmail from "validator/lib/isEmail";
+
+console.log(validator.isEmail("tharun.2.linux@gmail.com"));
