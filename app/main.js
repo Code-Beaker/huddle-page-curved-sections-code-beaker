cb587186f7ba81eba3456120a3bbcf7b38af543a
Tharun Raj <tharun.2.linux@gmail.com>
2024-05-29 18:14:26 +0530

    add fonts

--- app/main.js
+++ app/main.js
@@ -4,4 +4,16 @@ import "./style.css";
 import validator from "validator";
 import isEmail from "validator/lib/isEmail";
 
-console.log(validator.isEmail("tharun.2.linux@gmail.com"));
+const form = document.getElementById("form");
+const emailInput = document.getElementById("emailInput");
+const emailStateText = document.querySelector(".footer__error-text");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!validator.isEmail(emailInput.value)) {
+    emailStateText.classList.add("error-display");
+  } else {
+    emailStateText.classList.remove("error-display");
+  }
+});
